fea91c70e0d45cf8326d41de155cc1b1ceb1cccc
Abner Martins <abner.09.martins@gmail.com>
2022-09-22 13:59:01 -0300

    Initial commit from Create Next App

---
+++ next.config.js
@@ -0,0 +1,15 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true
+}
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const withPWA = require('next-pwa')({
+  dest: 'public'
+})
+
+module.exports = {
+  nextConfig,
+  withPWA
+}
